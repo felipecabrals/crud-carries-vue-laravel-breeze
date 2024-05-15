476d61f78bb432226ed0bac777083f75df97bae5
Felipe Dias <felipe.cabral@omnicog.com.br>
2024-05-15 09:31:53 -0300

    Inicializando projeto

---
+++ database/migrations/2024_05_15_122702_create_carries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carries', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('nome');
+            $table->string('endereco');
+            $table->string('bairro');
+            $table->string('cidade');
+            $table->string('uf');
+            $table->string('limite_credito');
+            $table->string('data_analise_credito');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carries');
+    }
+};
